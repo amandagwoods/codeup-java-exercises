9438feb8c2be3ac9b644a6ebecaacca833cae3f7
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-09-29 15:56:22 -0500

    notes after walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,6 +9,7 @@ public class ConsoleExercises {
         int age = sc.nextInt();
         System.out.printf("You are %s years old.\n", age);
 
+        //explore scanner class
         System.out.println("What are your 3 favorite colors?");
         String color1 = sc.next();
         String color2 = sc.next();
@@ -18,9 +19,18 @@ public class ConsoleExercises {
 
         System.out.println("What are your 3 favorite colors?");
         String faveColors = sc.nextLine();
-        System.out.printf("Wow! I really like %s too! They're great colors.", faveColors);
+        System.out.printf("Wow! I really like %s too! They're great colors.\n", faveColors);
 
+        //calculate area and perimeter of room
+        System.out.println("What is the width of the room?");
+        int length = Integer.parseInt(sc.nextLine());
+        System.out.println("What is the length of the room?");
+        int width = Integer.parseInt(sc.nextLine());
+        int area = length * width;
+        int perimeter = (2 * length) + (2 * width);
 
+        System.out.printf("The area is: %d.\n", area);
+        System.out.printf("The perimeter is: %d.\n", perimeter);
 
     }
 }
