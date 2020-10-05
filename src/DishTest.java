dbbcbde59d0640eef434519dcbbb8b8aa27d6c81
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-05 14:37:36 -0500

    completed dish oop mini lesson

---
+++ src/DishTest.java
@@ -0,0 +1,10 @@
+public class DishTest {
+    public static void main(String[] args) {
+        Dish d = new Dish ();
+        d.costInCents = 500;
+        d.nameOfDish = "ice cream";
+        d.wouldRecommend = true;
+        d.printSummary();
+
+    }
+}
