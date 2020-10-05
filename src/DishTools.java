b75be8e7fb37bc62923b2e573f97768d1ff774af
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-05 16:35:04 -0500

    feat: walkthrough

--- src/DishTools.java
+++ src/DishTools.java
@@ -1,9 +1,20 @@
 public class DishTools {
     public static void main(String[] args) {
-        int AVERAGE_COST_OF_DISH_IN_CENTS = 13000;
-         public String shoutDishName() {
-            return String toUpperCase("nameOfDish");
+        public static final int AVERAGE_COST_OF_DISH_IN_CENTS = 13000;
 
+        public static void shoutDishName(Dish d){
+            System.out.println(d.nameOfDish.toUpperCase());
+        }
+
+        public static void analyzeDishCost(Dish d){
+            String message = (d.costInCents < AVERAGE_COST_OF_DISH_IN_CENTS) ?
+                    "Less expensive than average" : "More expensive than average";
+            System.out.println(message);
+        }
+
+        public static Dish flipRecommendation(Dish d){
+            d.wouldRecommend = !d.wouldRecommend;
+            return d;
         }
     }
 }
