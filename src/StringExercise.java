2d3615c36bcd723241ce39e5dad98b72e1cfbd81
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-05 15:11:11 -0500

    completed exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,15 @@
 public class StringExercise {
     public static void main(String[] args) {
+        String song = "We don't need no education";
+        System.out.println(song);
+        song = song = "We don't need no thought control";
+        System.out.println(song);
+
+        String message = "Check \"this\" out!";
+        System.out.println(message);
+        message = message = "\"s inside of \"s!";
+        System.out.println(message);
+
 
     }
 }
