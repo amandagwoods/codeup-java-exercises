b3a49d9b331bdeb086cb08e06af51c4282ede7c9
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-06 09:48:50 -0500

    feat: in progress

--- src/DishTools.java
+++ src/DishTools.java
@@ -1,23 +1,23 @@
-public class DishTools {
-    public static void main(String[] args) {
-        public static final int AVERAGE_COST_OF_DISH_IN_CENTS = 13000;
-
-        public static void shoutDishName(Dish d){
-            System.out.println(d.nameOfDish.toUpperCase());
-        }
-
-        public static void analyzeDishCost(Dish d){
-            String message = (d.costInCents < AVERAGE_COST_OF_DISH_IN_CENTS) ?
-                    "Less expensive than average" : "More expensive than average";
-            System.out.println(message);
-        }
-
-        public static Dish flipRecommendation(Dish d){
-            d.wouldRecommend = !d.wouldRecommend;
-            return d;
-        }
-    }
-}
+//public class DishTools {
+//    public static void main(String[] args) {
+////        public static final int AVERAGE_COST_OF_DISH_IN_CENTS = 13000;
+//
+//        public static void shoutDishName(Dish d){
+//            System.out.println(d.nameOfDish.toUpperCase());
+//        }
+//
+//        public static void analyzeDishCost(Dish d){
+//            String message = (d.costInCents < AVERAGE_COST_OF_DISH_IN_CENTS) ?
+//                    "Less expensive than average" : "More expensive than average";
+//            System.out.println(message);
+//        }
+//
+//        public static Dish flipRecommendation(Dish d){
+//            d.wouldRecommend = !d.wouldRecommend;
+//            return d;
+//        }
+//    }
+//}
 
 //Create a class of static fields (properties and methods) called DishTools
 //      - AVERAGE_COST_OF_DISH_IN_CENTS - an integer constant set to 13000 (use the final keyword to make it a constant)
